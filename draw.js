a23318730e4cd8dbefe8968c4f6fadc38d66e8d5
Rudolf Ádám <sari.vogel.94@gmail.com>
2018-10-29 10:36:44 +0100

    devide parts of the code of the smiley

--- draw.js
+++ draw.js
@@ -3,19 +3,31 @@
 function draw() {
     var canvas = document.querySelector("canvas");
     var context = canvas.getContext("2d");
+
+    context.linewidth = 3;
+    context.strokeStyle = "red";
     
     context.beginPath();
-    context.arc(75, 75, 50, 0, 2*Math.PI);
+    drawHead(context);
+    drawEyes(context);
+    drawMouth(context);
     context.stroke();
- 
-    context.moveTo(110, 75);
-    context.arc(75, 75, 35, 0, Math.PI, false); 
+}
 
+function drawHead(context) {
+    context.arc(75, 75, 50, 0, 2*Math.PI);
+}
+
+function drawEyes(context) {
     context.moveTo(65, 65);
     context.arc(60, 65, 5, 0, Math.PI * 2, true); // Left eye
     context.moveTo(95, 65);
     context.arc(90, 65, 5, 0, Math.PI * 2, true); // Right eye
-    context.stroke();
+}
+
+function drawMouth(context) {
+    context.moveTo(110, 75);
+    context.arc(75, 75, 35, 0, Math.PI,); 
 }
 
 /*function draw() {
